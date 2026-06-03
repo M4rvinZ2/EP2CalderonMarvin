a86d63bce6378bc782927aebefd1bb754dabf355
M4rvinZ2 <marvindanielcalderonaa@gmail.com>
2026-06-03 11:39:45 -0600

    fix: actualizar base path a EP2CalderonMarvin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/SimulacionRobo/',
+  base: '/EP2CalderonMarvin/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
